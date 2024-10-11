410a232feb3733ea5d757c79e2b7eda1f7c4557a
Vikram <vikrammishra824@gmail.com>
2024-10-10 22:37:34 -0700

    Edited command

--- launcher.py
+++ launcher.py
@@ -37,9 +37,9 @@ class AppIcon:
 # Create app icons
 apps = [
     AppIcon(50, 50, 150, 150, CYAN_GREEN, "Eyes", "screensaver.py"),
-    AppIcon(250, 50, 150, 150, (100, 100, 255), "Orchard Nav", "python basic_pipelines/detection.py --labels-json resources/zaptrack-labels.json --hef zaptrack-2024-10-08.hef -i rpi"),
-    AppIcon(450, 50, 150, 150, BLUE, "Pose", ["rpicam-hello", "-t", "0s", "--post-process-file", "/usr/share/rpi-camera-assets/imx500_posenet.json", "--viewfinder-width", "1920", "--viewfinder-height", "1080", "--framerate", "30"]),
-    AppIcon(650, 50, 150, 150, PURPLE, "People", ["rpicam-hello", "-t", "0s", "--post-process-file", "/usr/share/rpi-camera-assets/imx500_mobilenet_ssd.json", "--viewfinder-width", "1920", "--viewfinder-height", "1080", "--framerate", "30"])
+    AppIcon(250, 50, 150, 150, (100, 100, 255), "Orchard Nav", "hailo/basic_pipelines/detection.py --labels-json hailo/resources/zaptrack-labels.json --hef zaptrack-2024-10-08.hef -i rpi"),
+    AppIcon(450, 50, 150, 150, BLUE, "Pose", "rpicam-hello -t 0s --post-process-file /usr/share/rpi-camera-assets/imx500_posenet.json --viewfinder-width 1920 --viewfinder-height 1080 --framerate 30"),
+    AppIcon(650, 50, 150, 150, PURPLE, "People", "rpicam-hello -t 0s --post-process-file /usr/share/rpi-camera-assets/imx500_mobilenet_ssd.json --viewfinder-width 1920 --viewfinder-height 1080 --framerate 30")
     
 ]
 
